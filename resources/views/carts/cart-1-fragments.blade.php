e6c9afd67ddd9a2c39075fe3914a0d5f8f9053f8
Aanselmo Velame <design@anselmovelame.com.br>
2019-04-29 11:16:27 -0300

    gitignore gitserver2

--- resources/views/carts/cart-1-fragments.blade.php
+++ resources/views/carts/cart-1-fragments.blade.php
@@ -6,7 +6,7 @@
     <div class="widget_shopping_cart_content">
         <ul class="woocommerce-mini-cart cart_list product_list_widget ">
             <li class="woocommerce-mini-cart-item mini_cart_item">
-                <a href="{{route('cart.remove')}}?remove_item=00a2829b91a876cd2c81122e0ccbd76d&_wpnonce=194ce587c0" class="remove remove_from_cart_button" aria-label="Remover este item" data-product_id="19531" data-cart_item_key="00a2829b91a876cd2c81122e0ccbd76d" data-product_sku="">&times;</a>
+                <a href="{{route('cart.remove')}}/?remove_item=00a2829b91a876cd2c81122e0ccbd76d&_wpnonce=194ce587c0" class="remove remove_from_cart_button" aria-label="Remover este item" data-product_id="19531" data-cart_item_key="00a2829b91a876cd2c81122e0ccbd76d" data-product_sku="">&times;</a>
                 <a href="#">
                     <img width="273" height="273" src="{{asset('faker/product_photos/img5-f.jpg')}}?ver=1.0.0" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" />
                     Produto 1
